3f5f45e78edef03189a0694620b01d7aa62352d4
Brian Chambers <brianchambers876@gmail.com>
2025-11-14 16:01:53 -0500

    Fix string interpolation in console logs

--- index.js
+++ index.js
@@ -12,30 +12,31 @@
 // Declare a variable named studentName and store your name as a string.
 // Use string interpolation to log the sentence describing its value and type.
 let studentName ="Yoltzin gonzalez";
-console.log (The value stored in the variable studentName is ${studentName} and its data type is ${typeof studentName}.)
+console.log (`The value stored in the variable studentName is ${studentName} and its data type is ${typeof studentName}.`)
 // 2️⃣ Number
 // Declare a variable named studentAge and store your age as a number.
 // Use string interpolation to log the sentence describing its value and type.
 let studentAge = 16;
-console.log(The value stored in the variable studentAge is ${studentAge} and its data type is ${typeof studentAge}.)
+console.log(`The value stored in the variable studentAge is ${studentAge} and its data type is ${typeof studentAge}.`)
 // 3️⃣ Boolean
 // Declare a variable named isEnrolled and assign it either true or false.
 // Use string interpolation to log the sentence describing its value and type.
 let isEnrolled = true;
-console.log(The value stored in the variable isEnrolled is ${isEnrolled} and its data type is ${typeof isEnrolled}.)
+console.log(`The value stored in the variable isEnrolled is ${isEnrolled} and its data type is ${typeof isEnrolled}.`)
 // 4️⃣ Undefined
 // Declare a variable named favoriteSubject but do not assign it a value.
 // Use string interpolation to log the sentence describing its value and type.
 let favoriteSubject;
-console.log(The value stored in the variable favoriteSubject is ${favoriteSubject} and its data type is ${typeof favoriteSubject}.)
+console.log(`The value stored in the variable favoriteSubject is ${favoriteSubject} and its data type is ${typeof favoriteSubject}.`)
 // 5️⃣ Null
 // Declare a variable named nextClass and set it equal to null.
 // Use string interpolation to log the sentence describing its value and type.
 let nextClass = null
-console.log(The value stored in the variable nextClass is ${nextClass} and its data type is ${typeof nextClass}.)
+console.log(`The value stored in the variable nextClass is ${nextClass} and its data type is ${typeof nextClass}.`)
 // 6️⃣ Check
 // When finished, run your file to verify that all outputs print correctly.
 // Run your file by typing node index.js in the terminal
 
 // 6️⃣ Commit & Push
 // Save your work and push it for grading
+
